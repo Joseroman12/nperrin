712dc48a556860b5e5017c0db6acc4f0c7f97bc2
normanperrin <norman.perrin.94@gmail.com>
2019-07-04 19:37:53 -0300

    add nonsense decoration to build script

--- buid.sh
+++ buid.sh
@@ -14,10 +14,10 @@ final_append_content='
 </html>
 '
 
-# pre build
+# --- pre build ---
 rm -fr ./public/ && mkdir public && cp -R ./static/* ./public
 
-# markdown to html
+#  --- markdown to html ---
 build_from_path () {
     path="$(echo "$1" |sed 's/data/public/;s/\.md/\.html/')"
     mkdir -p "$(dirname "$path")"
@@ -25,7 +25,7 @@ build_from_path () {
 }
 find data/* -name '*.md' |while read -r file; do build_from_path "$file"; done
 
-# create html files for directories
+# --- create html files for directories ---
 create_html_file_list () {
     item_date="$(echo "$1" |sed -E 's/(\-)?[a-z].*//')"
     item_name="$item_date - $(echo "$1" |sed -E 's/([0-9]*\-)*//' )"
@@ -33,19 +33,16 @@ create_html_file_list () {
             <a href=\"./$1\">${item_name# - }</a>
         </li>"
 }
-
 create_html_dir_list () {
     echo "        <li>
             <a href=\"./$1\">$1</a>
         </li>"
 }
-
 create_html_from_directory () {
-    html_list="$(ls -rp "$1" |grep -v / | grep -v 404 |while read -r file; do create_html_file_list "$file"; done)"
+    html_list="$(ls -rp "$1" |grep -v / |grep -v 404 |while read -r file; do create_html_file_list "$file"; done)"
     dir_list="$(ls -rp "$1" |grep / |while read -r dir; do create_html_dir_list "$dir"; done)"
     cp directory.html "$1/index.html"
     append_content="$html_list$files_append_content$dir_list$final_append_content"
     echo "$append_content" >> "$1/index.html"
 }
-# find public -type d |while read f; do create_html_from_directory "$f"; done
 find public -type d |while read -r node; do create_html_from_directory "$node"; done
