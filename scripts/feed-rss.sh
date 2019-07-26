6be2a3639b10f9800d7dec0aa5bceef4db359e9b
normanperrin <nperrin@riseup.net>
2019-07-26 19:30:44 -0300

    add category and doc

--- scripts/feed-rss.sh
+++ scripts/feed-rss.sh
@@ -11,11 +11,15 @@ find data -type f ! -name '404.md' ! -name 'index.md' |while read -r path; do li
 feed_list="$(sort -r .temp-rss |head -n 10)"
 rm .temp-rss
 
-printf '<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n\t<channel>\n\t\t<title>Norman Perrin</title>\n\t\t<link>https://nperrin.io</link>\n\t\t<description>Recent content from Norman Perrin</description>\n\t\t<language>en</language>\n\t\t<image>\n\t\t\t<url>https://nperrin.io/img/me.jpeg</url>\n\t\t\t<title>Norman Perrin</title>\n\t\t\t<link>https://nperrin.io</link>\n\t\t\t<width>140</width>\n\t\t\t<height>140</height>\n\t\t</image>\n\t\t<lastBuildDate>Thu, 25 Jul 2019 21:05:29 -0300</lastBuildDate>\n\t\t<atom:link href=\"https://nperrin.io/feed/index.xml\" rel=\"self\" type=\"application/rss+xml\"/>\n' > "$output_rss"
+printf '<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n\t<channel>\n\t\t<title>Norman Perrin</title>\n\t\t<link>https://nperrin.io</link>\n\t\t<docs>http://backend.userland.com/rss</docs>\n\t\t<description>Recent content from Norman Perrin</description>\n\t\t<language>en</language>\n\t\t<image>\n\t\t\t<url>https://nperrin.io/img/me.jpeg</url>\n\t\t\t<title>Norman Perrin</title>\n\t\t\t<link>https://nperrin.io</link>\n\t\t\t<width>140</width>\n\t\t\t<height>140</height>\n\t\t</image>\n\t\t<lastBuildDate>Thu, 25 Jul 2019 21:05:29 -0300</lastBuildDate>\n\t\t<atom:link href=\"https://nperrin.io/feed/index.xml\" rel=\"self\" type=\"application/rss+xml\"/>\n' > "$output_rss"
 
 map_date () {
 	timestamp="$(date -j -f '%Y/%m/%d %H:%M:%S' '+%s' "$1 00:00:00")"
 	date -r "$timestamp" '+%a, %d %b %Y %H:%M:%S %z'
+}
+
+get_category () {
+	capitalize "$(basename "$(dirname "$1")" |sed 's/data/Main/')"
 }
 
 rss_item () {
@@ -27,9 +31,10 @@ rss_item () {
 	description="$(printf '\t\t\t<description>%s</description>' "$(grep '^description:' "$path" |sed -E 's/description: (.*)$/\1/')")"
 	pubDate="$(printf '\t\t\t<pubDate>%s</pubDate>' "$compliant_date")"
 	guid="$(printf '\t\t\t<guid>https://nperrin.io%s</guid>' "$(grep '^url:' "$path" |sed -E 's/url: (.*)/\1/')")"
+	category="$(printf '\t\t\t<category>%s</category>' "$(get_category "$path")")"
 	source="$(printf '\t\t\t<source url=\"https://nperrin.io/feed/index.xml\">nperrin</source>')"
 
-	printf '\t\t<item>\n%s\n%s\n%s\n%s\n%s\n%s\n\t\t</item>\n' "$title" "$link" "$description" "$pubDate" "$guid" "$source" >> "$output_rss"
+	printf '\t\t<item>\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\t\t</item>\n' "$title" "$link" "$description" "$category" "$pubDate" "$guid" "$source" >> "$output_rss"
 }
 
 printf '%s' "$feed_list" |while read -r line; do rss_item "$line"; done
