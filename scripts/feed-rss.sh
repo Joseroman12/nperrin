6e90ad99f5f7db2994bcf16bb496f206f70040c5
normanperrin <nperrin@riseup.net>
2019-07-26 18:47:07 -0300

    fix rss validator issues

--- scripts/feed-rss.sh
+++ scripts/feed-rss.sh
@@ -11,7 +11,7 @@ find data -type f ! -name '404.md' ! -name 'index.md' |while read -r path; do li
 feed_list="$(sort -r .temp-rss |head -n 10)"
 rm .temp-rss
 
-printf '<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n\t<channel>\n\t\t<title>nperrin</title>\n\t\t<link>https://nperrin.io</link>\n\t\t<description>Recent content from Norman Perrin</description>\n\t\t<language>en</language>\n\t\t<image>\n\t\t\t<url>https://nperrin.io/img/me.jpeg</url>\n\t\t\t<title>Norman Perrin blurred</title>\n\t\t\t<link>https://nperrin.io</link>\n\t\t\t<width>140</width>\n\t\t\t<height>140</height>\n\t\t</image>\n\t\t<managingEditor>nperrin</managingEditor>\n\t\t<webMaster>nperrin</webMaster>\n\t\t<lastBuildDate>Thu, 25 Jul 2019 21:05:29 -0300</lastBuildDate>\n\t\t<atom:link href=\"https://nperrin.io/feed/index.xml\" rel=\"self\" type=\"application/rss+xml\"/>\n' > "$output_rss"
+printf '<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n\t<channel>\n\t\t<title>Norman Perrin</title>\n\t\t<link>https://nperrin.io</link>\n\t\t<description>Recent content from Norman Perrin</description>\n\t\t<language>en</language>\n\t\t<image>\n\t\t\t<url>https://nperrin.io/img/me.jpeg</url>\n\t\t\t<title>Norman Perrin</title>\n\t\t\t<link>https://nperrin.io</link>\n\t\t\t<width>140</width>\n\t\t\t<height>140</height>\n\t\t</image>\n\t\t<lastBuildDate>Thu, 25 Jul 2019 21:05:29 -0300</lastBuildDate>\n\t\t<atom:link href=\"https://nperrin.io/feed/index.xml\" rel=\"self\" type=\"application/rss+xml\"/>\n' > "$output_rss"
 
 map_date () {
 	timestamp="$(date -j -f '%Y/%m/%d %H:%M:%S' '+%s' "$1 00:00:00")"
@@ -25,7 +25,7 @@ rss_item () {
 	title="$(printf '\t\t\t<title>%s</title>' "$(grep '^title:' "$path" |sed -E 's/title: (.*)/\1/')")"
 	link="$(printf '\t\t\t<link>https://nperrin.io%s</link>' "$(grep '^url:' "$path" |sed -E 's/url: (.*)/\1/')")"
 	description="$(printf '\t\t\t<description>%s</description>' "$(grep '^description:' "$path" |sed -E 's/description: (.*)$/\1/')")"
-	pubDate="$(printf '\t\t\t<pubDate>%s</pubDate>"' "$compliant_date")"
+	pubDate="$(printf '\t\t\t<pubDate>%s</pubDate>' "$compliant_date")"
 	guid="$(printf '\t\t\t<guid>https://nperrin.io%s</guid>' "$(grep '^url:' "$path" |sed -E 's/url: (.*)/\1/')")"
 	source="$(printf '\t\t\t<source url=\"https://nperrin.io/feed/index.xml\">nperrin</source>')"
 
