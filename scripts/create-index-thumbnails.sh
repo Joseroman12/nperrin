e9b3762f9d8c8a38f540459a47a469dd9dbf58e7
normanperrin <nperrin@riseup.net>
2019-08-03 01:01:13 -0300

    change thumbnails

--- scripts/create-index-thumbnails.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-path="$1"
-text="$2"
-
-out_path="$(dirname "$path" |sed 's@data@static\/img@')/$(echo "$text" |sed 's/ /-/g').png"
-
-convert -background '#222222' -font Verdana -pointsize 80 -fill '#F3F3F3' label:"$text" -gravity southeast -splice 120x120 -gravity northwest -splice 120x120 "$out_path"
-
